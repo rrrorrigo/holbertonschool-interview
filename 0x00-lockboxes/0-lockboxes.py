febb0cfd29368b7a83afc1e2056aead13fac1948
rrrorrigo <rodrigobriones54@gmail.com>
2022-04-21 20:50:22 -0300

    added task 0

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""LOCKBOXES"""
+
+
+def canUnlockAll(boxes):
+    """ method that determines if all the boxes can be opened"""
+    keys = set(boxes[0])
+    pichu = 0
+    check = set([])
+    if len(keys) <= 0:
+        return(False)
+    for i in range(0, len(boxes)):
+        if len(boxes[i]) <= 0:
+            pichu += 1
+            continue
+        elif i in keys:
+            keys.update(boxes[i])
+    for i in range(0, len(boxes)):
+        if len(boxes[i]) <= 0:
+            pichu += 1
+            continue
+        elif i in keys:
+            keys.update(boxes[i])
+    for i in range(0, len(boxes)):
+        check.add(i)
+    if 0 in keys:
+        keys.remove(0)
+    if 0 in check:
+        check.remove(0)
+    if keys == check:
+        return True
+    else:
+        return False
