41ed2ed1246c0a1c6e77548c2bb5651f639bc921
rrrorrigo <rodrigobriones54@gmail.com>
2022-08-18 23:07:49 -0300

    added task 1

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -9,8 +9,20 @@ def rain(walls):
     function that estimates how much water can be retained between
     single/stacked walls represented in a list of walls
     """
-    water = 0
-    for i in range(len(walls) - 2):
-        if walls[i] and walls[i + 2]:
-            water += min(walls[i], walls[i + 2])
-    return
+    raindrops = 0
+    if len(walls) < 1:
+        return 0
+    for i in range(len(walls)):
+        # Find the maximum element on its left
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+
+        # Find the maximum element on its right
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+
+        # Update the maximum water
+        raindrops += min(left, right) - walls[i]
+    return raindrops
